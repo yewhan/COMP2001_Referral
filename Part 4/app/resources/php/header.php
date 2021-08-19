c3d304c69fcc0f0c88fe494b8f9c974a724bd044
exagt <23314220+exagt@users.noreply.github.com>
2021-08-19 14:37:10 +0100

    edited css

--- Part 4/app/resources/php/header.php
+++
@@ -1,8 +0,0 @@
-<div class="header">
-    <h1>Plymouth Air Quality</h1>
-
-    <ul class="navigation">
-        <a href="../app/index.php">Home</a>
-        <a href="../app/data.php">Air Quality Data</a>
-    </ul>
-</div>
